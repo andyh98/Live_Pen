42baa1a4498004b123eace7e6996f63bf561a3f4
andyh98 <hameea1@mcmaster.ca>
2018-08-03 21:08:38 -0400

    Added Palette

--- javascripts/draw_board.js
+++ javascripts/draw_board.js
@@ -3,9 +3,19 @@
 var drawingCanvas = document.getElementById('drawing_board');
 var cdraw = drawingCanvas.getContext('2d');
 
+//Mouse is clicked down and being dragged
+var dragging = false;
+
+//line thickness is twice the radius
+var radius = 5;
+
+//global variable will be manipulated
+var colorChoice = "#D05237";
+
+
 //drawing tool ---> circle created as mouse hovers
-function Circle (x, y, color, radius){
-  this.color = color;
+function Circle (x, y, radius){
+  this.color = colorChoice;
   this.radius = radius;
   this.x = x;
   this.y = y;
@@ -18,15 +28,41 @@ function Circle (x, y, color, radius){
   }
 }
 
+var engage = function(e) {
+  dragging = true;
+  point(e);
+}
+
+var disengage = function(){
+  dragging = false;
+  //stop line connections
+  cdraw.beginPath();
+}
+
+var point = function(event) {
+
+  if(click == 1 && dragging){
+    cdraw.lineTo(event.offsetX, event.offsetY);
+    cdraw.stroke();
+    new Circle(event.offsetX, event.offsetY, radius).draw();
+    cdraw.beginPath();
+    cdraw.moveTo(event.offsetX, event.offsetY);
+  }
+}
+
 //setting width and height of the drawing canvas
 drawingCanvas.width = 700;
 drawingCanvas.height = 400;
 // drawingCanvas.translate(500, 500);
 
-drawingCanvas.addEventListener('mousemove', function(event) {
-  if(click == 1)
-    return new Circle(event.offsetX, event.offsetY , "#D05237", 5).draw();
-});
+//custom "while mouse is down" listener
+drawingCanvas.addEventListener('mousedown', engage);
+drawingCanvas.addEventListener('mouseup', disengage);
+
+cdraw.lineWidth = 2*radius;
+cdraw.strokeStyle = "#D05237";
+//create circles when mouse is down
+drawingCanvas.addEventListener('mousemove', point);
 
 
 //clearing the canvas
